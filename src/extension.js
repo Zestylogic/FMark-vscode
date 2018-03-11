c9d6102683d63ec04ad40630f24f52e4b97373df
Yann Herklotz <ymherklotz@gmail.com>
2018-03-11 19:59:03 +0000

    Kinda working

--- src/extension.js
+++ src/extension.js
@@ -5,13 +5,33 @@ const finterop = require('./fmarkinterop')
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
+    const contentProvider = new finterop.FMarkContentProvider(context);
+    const contentProviderRegistration = vscode.workspace.registerTextDocumentContentProvider('fmark-preview', contentProvider);
+
+    // vscode.workspace.openTextDocument(customUri)
+    // .then(function(doc) {
+    //     // show document in a new editor in a new tab
+    //     return vscode.window.showTextDocument(doc, 
+    //         vscode.ViewColumn.Two, true);
+    // });
+
     context.subscriptions.push(vscode.commands.registerCommand('fmark.openPreview', finterop.openPreview));
-    context.subscriptions.push(vscode.commands.registerCommand('fmark.recompile', finterop.recompileAllFMark));
+
+    context.subscriptions.push(vscode.commands.registerCommand('fmark.recompile', contentProvider.updateFMark('')));
+
     context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(document => {
-        if(finterop.isFMarkFile(document)) {
-            finterop.recompileFMarkFile(document.uri);
+        if (finterop.isFMarkFile(document)) {
+            contentProvider.updateFMark(document);
         }
     }));
+
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument((event) => {
+        if (finterop.isFMarkFile(event.document)) {
+			contentProvider.update(event.document)
+		}
+    }));
+
+    context.subscriptions.push(contentProviderRegistration);
 }
 
 // this method is called when your extension is deactivated
--- src/fmarkinterop.js
+++ src/fmarkinterop.js
@@ -1,22 +1,62 @@
 const vscode = require('vscode');
+const fmark = require('./FMark/FMark/fmark.js')
 
-function isFMarkFile(document) {
-    return document.languageId === 'fmark';
+var singlePreviewSourceUri = null;
+
+class FMarkContentProvider {
+
+    constructor (context) {
+        this._onDidChange = new vscode.EventEmitter();
+        this._waiting = false;
+        this.content = "nothing";
+    }
+
+    provideTextDocumentContent(previewUri) {
+        console.log(this.content)
+        return this.content;
+    }
+
+    get onDidChange() {
+        return this._onDidChange.event;
+    }
+
+    updateFMark(uri) {
+        //prevUri = getPreviewUri(previewUri);
+        console.log("hello")
+        uri = vscode.window.activeTextEditor.document.uri;
+        this.content = vscode.workspace.openTextDocument(uri).then((doc) => {
+            return fmark.processMarkdownString(doc.getText()).data;
+        });
+        return this._onDidChange.fire(getPreviewUri(''));
+    }
+
+    update(uri) {
+        if(!this.waiting) {
+            this.waiting = true;
+            setTimeout(() => {
+                this.waiting = false;
+                this.updateFMark(uri);
+            }, 300);
+        }
+    }
 }
 
-function openPreview(uri) {
-    console.log('Opened preview for FMark.');
-    console.log('File location: ' + uri.toString());
+function isFMarkFile(document) {
+    return document.languageId === 'fmark' && document.uri.scheme !== 'fmark-preview';
+}
+
+function openPreview() {
     return vscode.commands.executeCommand(
         'vscode.previewHtml',
-        uri,
+        vscode.Uri.parse('fmark-preview://single-preview.rendered'),
         vscode.ViewColumn.Two,
         'Preview'
     );
 }
 
-function recompileFMarkFile(uri) {
-    console.log('Recompiling: ' + uri.toString());
+function recompileFMarkFile(document) {
+    console.log('Recompiling: ' + document.uri.toString());
+    console.log(fmark.processMarkdownString(document.getText()));
 }
 
 function recompileAllFMark() {
@@ -24,13 +64,30 @@ function recompileAllFMark() {
         console.log("Recompiling everything")
         if(isFMarkFile(document)) {
             console.log("Found fmark file at: " + document.uri)
-            recompileFMarkFile(document.uri);
+            recompileFMarkFile(document);
         }
     });
 }
 
+function getPreviewUri(uri) {
+    if (uri.scheme === 'fmark-preview') {
+        return uri;
+    }
+
+    singlePreviewSourceUri = uri;
+
+    var previewUri = uri;
+    previewUri.scheme = 'fmark-preview';
+    previewUri.path = 'single-preview.rendered';
+
+    previewUri = vscode.Uri.parse('fmark-preview://single-preview.rendered');
+
+    return previewUri;
+}
+
 module.exports = {
+    FMarkContentProvider,
     openPreview,
-    recompileFMarkFile,
-    recompileAllFMark,
+    isFMarkFile,
+    getPreviewUri,
 }
